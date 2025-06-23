0920d9102d74f9f21b58bc80f04556dd5daf4113
Taanveer22 <taanveer469@gmail.com>
2025-06-23 19:34:47 +0600

    alhamdulliah savings formula applied and amount showed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 console.log("script js connected");
 
+// -------------------------------------------------
+//event listener for calculate button
+// -------------------------------------------------
+
 const calculateButton = document.getElementById("calculate");
 // console.log(calculateButton);
 
 calculateButton.addEventListener("click", function () {
-  console.log("calculate button clicked");
+  // console.log("calculate button clicked");
 
   const income = parseFloat(document.getElementById("income").value);
   const software = parseFloat(document.getElementById("software").value);
@@ -13,7 +17,7 @@ calculateButton.addEventListener("click", function () {
 
   // console.log({ income, software, courses, internet });
   // console.dir({ income, software, courses, internet });
-  console.table({ income, software, courses, internet });
+  // console.table({ income, software, courses, internet });
 
   const totalExpenses = software + courses + internet;
   const balance = income - totalExpenses;
@@ -23,4 +27,36 @@ calculateButton.addEventListener("click", function () {
 
   const balanceElement = document.getElementById("balance");
   balanceElement.innerText = balance.toFixed(2);
+
+  const resultsElement = document.getElementById("results");
+  resultsElement.classList.remove("hidden");
+});
+
+// -------------------------------------------------
+//event listener for calculate savings button
+// -------------------------------------------------
+const calculateSavingsButton = document.getElementById("calculate-savings");
+// console.log(calculateSavingsButton);
+calculateSavingsButton.addEventListener("click", function () {
+  // console.log('savings button clicked');
+  const income = parseFloat(document.getElementById("income").value);
+  const software = parseFloat(document.getElementById("software").value);
+  const courses = parseFloat(document.getElementById("courses").value);
+  const internet = parseFloat(document.getElementById("internet").value);
+
+  console.table({ income, software, courses, internet });
+
+  const totalExpenses = software + courses + internet;
+  const balance = income - totalExpenses;
+
+  const savingsPercentageInput = parseFloat(
+    document.getElementById("savings-percentage-input").value
+  );
+  // console.log(savingsPercentageInput);
+
+  const savingsFormula = (savingsPercentageInput * balance) / 100;
+  // console.log(savingsFormula);
+
+  const savingsAmountElement = document.getElementById("savings-amount");
+  savingsAmountElement.innerText = savingsFormula;
 });
