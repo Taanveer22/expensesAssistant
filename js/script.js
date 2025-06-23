fd2ef5f261822999b57ea37dcd19c829749406ef
Taanveer22 <taanveer469@gmail.com>
2025-06-23 16:57:30 +0600

    alhamdulliah event listener added and input field value showed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('script js connected');
+console.log("script js connected");
+
+const calculateButton = document.getElementById("calculate");
+// console.log(calculateButton);
+
+calculateButton.addEventListener("click", function () {
+  console.log("calculate button clicked");
+
+  const income = parseFloat(document.getElementById("income").value);
+  const software = parseFloat(document.getElementById("software").value);
+  const courses = parseFloat(document.getElementById("courses").value);
+  const internet = parseFloat(document.getElementById("internet").value);
+
+  // console.log({ income, software, courses, internet });
+  // console.dir({ income, software, courses, internet });
+  console.table({ income, software, courses, internet });
+});
